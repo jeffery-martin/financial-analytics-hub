234603b8c7f9f3bd6f2a68c6973ba3c6f8b7a77e
Jeff Martin <jeffery.tyler.martin@gmail.com>
2025-08-23 00:15:12 -0700

    Update bot configuration with new credentials

--- projects/databricks-genie-multiagent/azure-teams-deployment/config.py
+++ projects/databricks-genie-multiagent/azure-teams-deployment/config.py
@@ -11,11 +11,11 @@ class DefaultConfig:
     """Bot Configuration"""
 
     PORT = 3978
-    APP_ID = "YourAppId" # This is the application ID for the bot service.
-    APP_PASSWORD = "YourAppPAssword"  # This is the password for the bot service.
+    APP_ID = "6db74ee0-d9eb-437f-84e6-067b873cf115" # This is the application ID for the bot service.
+    APP_PASSWORD = "4_K8Q~~x4XiHAEe6o6jOoNSMSBQYsr1Kuco1UcUh"  # This is the password for the bot service.
     APP_TYPE = "SingleTenant" # "SingleTenant" or "MultiTenant"
     #APP_TYPE = "MultiTenant" You can use this if testing locally
-    APP_TENANTID = "YourTenantID" # This is the tenant ID for the bot service.
-    DATABRICKS_SPACE_ID="YourSpaceID"
-    DATABRICKS_HOST="https://yourdatabricksworkspace.cloud.databricks.com"
-    DATABRICKS_TOKEN="YourDatabricksToken"
+    APP_TENANTID = "da77ffb1-ac97-492b-9f99-aec375868c10" # This is the tenant ID for the bot service.
+    DATABRICKS_SPACE_ID="01f052b47d04167a84e7e866d4316efc"
+    DATABRICKS_HOST="mdt-diabetes-dip-analytics-prod.cloud.databricks.com"
+    DATABRICKS_TOKEN="dapi4ba8d9f34cf4850819c20bbcbe4bc530"
